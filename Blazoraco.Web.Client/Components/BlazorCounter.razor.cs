dba70bb1d250145ada986ab46dd3b66f4059a513
Leks ter Schuur <leks@leksts.com>
2024-03-13 20:25:12 +0100

    Add interactive component

---
+++ Blazoraco.Web.Client/Components/BlazorCounter.razor.cs
@@ -0,0 +1,8 @@
+namespace Blazoraco.Web.Client.Components;
+
+public sealed partial class BlazorCounter
+{
+    private int currentCount = 0;
+
+    private void IncrementCount() => currentCount++;
+}
